d032a0a88a5a84ef4bf87324e486f2e3bfabfd69
Phobos <onabanjt@gmail.com>
2026-07-24 19:36:32 -0700

    feat: add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Navbar from "./components/Navbar";
+import Landing from "./pages/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 export default function Navbar() {
     return(
-        <nav className="flex flex-row bg-[#9550a7] top-0 fixed w-full h-fit py-4 px-8 justify-between items-center">
-            <img src="logo.png" alt="logo" className="h-20 w-auto"/>
-            <ul className="flex flex-row gap-5 text-white text-[17px]">
-                <a href="">Home</a>
+        <nav className="flex flex-row bg-[#9550a7] top-0 z-10 fixed w-full h-fit px-20 justify-between items-center">
+            <img src="logo.png" alt="logo" className="h-32 object-contain"/>
+            <ul className="flex flex-row gap-5 text-white font-inter text-[20px]">
+                <a href="">Home</a> 
                 <a href="">Posibility UK</a>
                 <a href="">Give</a>
             </ul>
